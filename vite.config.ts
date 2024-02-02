93376e92ee40e75e44cbbdfebb0c096293af49f4
Vishrut Reddi <vishrutreddi@gmail.com>
2024-02-02 15:58:11 -0800

    Update (#34)

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,20 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src/"),
+      "@assets": path.join(__dirname, "src/assets"),
+      "@components": path.join(__dirname, "src/components"),
+      "@utils": path.join(__dirname, "src/utils"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
